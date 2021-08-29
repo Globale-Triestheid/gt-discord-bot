ad0e204174aeef9d5f9333add9f5c775253d3e78
kai-353 <70913300+kai-353@users.noreply.github.com>
2021-08-29 19:49:30 +0200

    Added deepfrying

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ from discord.ext import commands
 # import asyncio
 # import random
 import os
+import requests
+from webscraper import Fryer
+from time import sleep
 
 from PIL import Image
 from io import BytesIO
@@ -30,5 +33,43 @@ async def wth(ctx, user: discord.Member = None):
     wanted_image.save("profile.jpg")
 
     await ctx.send(file=discord.File("profile.jpg"))
+
+@client.command()
+async def deepfry(ctx, user: discord.Member = None):
+    print(ctx.message.reference)
+    await ctx.message.add_reaction(u"\U0001F595")
+
+    if not user:
+        if not ctx.message.reference:
+            await ctx.send("Pls reply to an image to deepfry with this command")
+            return
+
+        ref_message = await ctx.fetch_message(ctx.message.reference.message_id)
+
+        if not ref_message.attachments:
+            await ctx.send("Pls reply to an image to deepfry with this command")
+            return
+
+        print(ref_message.attachments[0].url)
+        url = ref_message.attachments[0].url
+
+        if not url.endswith((".jpg", ".png", ".jpeg")):
+            await ctx.send("Pls reply to an image to deepfry with this command")
+            return
+
+        res = requests.get(url)
+        img = Image.open(BytesIO(res.content))
+    else:
+        asset = user.avatar_url_as(size=512)
+
+        data = BytesIO(await asset.read())
+        img = Image.open(data)
+
+    img = img.convert("RGB")
+    img.save("deepfry.jpg")
+    path = os.path.dirname(os.path.realpath('deepfry.jpg')) + "\deepfry.jpg"
+    print(path)
+    Fryer().deepFry(image_url=path)
+    await ctx.send(file=discord.File("deep_img.png"))
 
 client.run(os.getenv("TOKEN"))
