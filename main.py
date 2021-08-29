c3f97934395edbc144870e45af15e008aa8eb60a
RotKopXL <djdemenezes@live.com>
2021-08-29 17:38:26 +0200

    wasted toegevoegt

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from discord.ext import commands
 # import random
 import os
 
-from PIL import Image
+from PIL import Image, ImageEnhance
 from io import BytesIO
 
 client = commands.Bot(command_prefix="!")
@@ -49,10 +49,40 @@ async def keemstar(ctx, user: discord.Member = None):
     keemstar_image = keemstar_image.convert("RGB")
     pfp.paste(keemstar_image, (0, 0))
 
-    pfp.save("profile.jpg")
+    pfp.save("profile_keemstar.jpg")
 
-    await ctx.send(file=discord.File("profile.jpg"))
+    await ctx.send(file=discord.File("profile_keemstar.jpg"))
+
+
+@client.command()
+async def wasted(ctx, user: discord.Member = None):
+    if not user:
+        user = ctx.author
+
+    print(user)
+
+    wasted_image = Image.open("wasted.jpg")
+
+    asset = user.avatar_url_as(size=512)
+    data = BytesIO(await asset.read())
+    pfp = Image.open(data)
+
+    enhancer = ImageEnhance.Brightness(pfp)
+
+    factor = 0.5  # darkens the image
+    pfp = enhancer.enhance(factor)
+
+    wasted_image = Image.open("wasted.jpg")
+
+    pfp.paste(wasted_image, (0, 200))
+
+
+    pfp.save("profile_wasted.jpg")
+
+    await ctx.send(file=discord.File("profile_wasted.jpg"))
+
 
 
 
 client.run(os.getenv("TOKEN"))
+
